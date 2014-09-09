0c5269d66a37394d7252ee748721bf8bd5746702
Dave Rice <dave@dericed.com>
2014-09-09 09:31:09 -0400

    dpdpscripts: fix typo

--- dpdpscripts.rb
+++ dpdpscripts.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Dpdpscripts < Formula
   homepage 'http://github.com/bavc/dpdpscripts'
   url 'http://github.com/dericed/dpdpscripts/archive/v0.1.zip'
-  sha '1d1f9fbcf0714a9f960fa8441630b9932a32f821615556512583ee28708f26eb'
+  sha256 '1d1f9fbcf0714a9f960fa8441630b9932a32f821615556512583ee28708f26eb'
   head 'git://github.com/bavc/dpdpscripts.git'
 
   depends_on 'bagit'
