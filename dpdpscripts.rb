da5ef9eaf6dc5958296fb59049aaf7cedbf162da
Dave Rice <dave@dericed.com>
2019-01-18 15:52:11 -0500

    dependency update

--- dpdpscripts.rb
+++ dpdpscripts.rb
@@ -9,11 +9,11 @@ class Dpdpscripts < Formula
   depends_on 'bagit'
   depends_on 'dvdauthor'
   depends_on 'exiftool'
-  depends_on 'ffmpeg' => [ 'with-ffplay', 'with-openjpeg', 'with-libass', 'with-freetype', 'with-faac' ]
+  depends_on 'ffmpeg' => [ 'with-openjpeg', 'with-libass', 'with-freetype' ]
   depends_on 'md5deep'
   depends_on 'media-info'
   depends_on 'xmlstarlet'
-  depends_on 'dericed/mm/mm'
+  depends_on 'mediamicroservices/mm/mm'
 
   def install
     bin.install "dpdpconfig"
